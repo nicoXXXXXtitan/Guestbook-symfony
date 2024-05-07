73e682858e0e53f976a94182c3e044ce106e60b3
nico <nicoc2010@gmail.com>
2024-05-07 17:59:19 +0200

    new homepage + page conference (controler + template).

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,22 +2,33 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Twig\Environment;
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/', name: 'app_conference')]
-    public function index(): Response
+    #[Route('/', name: 'homepage')]
+    public function index(Environment $twig, ConferenceRepository $conferenceRepository): Response
     {
-        return new Response(<<<EOF
-                    <html>
-                        <body>
-                            <img src="/images/under-construction.gif" />
-                        </body>
-                    </html>
-                    EOF
-        );
+        return new Response($twig->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]));
+    }
+
+    #[Route('/conference/{id}', name:'conference')]
+    public function show(Environment $twig, Conference $conference, CommentRepository $commentRepository) : Response
+    {
+        return new Response($twig->render('conference/show.html.twig', [
+            'conference'  => $conference,
+            'comments'   => $commentRepository->findBy([
+                'conference' => $conference,
+                'createdAt' => 'DESC'
+                ])
+        ]));
     }
 }
